86ebcb6338febc973d3d00c6ae06086ae343c31f
Tesear <59779985+Tesear@users.noreply.github.com>
2022-04-08 09:31:48 +0200

    Update

--- src/ngui.java
+++ src/ngui.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class ngui {
@@ -7,7 +8,7 @@ public class ngui {
 
     ngui(){
         JFrame frame = new JFrame();
-        frame.setTitle("Pup");
+        frame.setTitle("GameList");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(500, 400));
         frame.add(panel1);
@@ -15,6 +16,21 @@ public class ngui {
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
-
+        list();
+    }
+    public void list(){
+        ArrayList<ArrayList> array_double = new ArrayList();
+        Object[][] arrr = new Object[0][];
+        try {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM GAMELists");
+            /*while (resultSet.next()){
+                System.out.println(resultSet.getString("Name"));
+            }*/
+        }
+        catch (SQLException ex){
+            System.out.print("failed", ex);
+        }
+        Object[][] inhalt = {{"Minecraft1", "Elon Musk", "2001"}, {"Minecraft 2", "Beff Jezos", "2002"}};
+        table1.setModel(new DefaultTableModel(inhalt, new String[]{"Name", "Publisher", "Jahr"}));
     }
 }
--- src/table.java
+++ src/table.java
@@ -10,8 +10,10 @@ public class table {
          Statement statement = null;
          String GameN = "Grape";
          String PubN = "Mojang";
+         int GenN = 1;
+         int YearN = 2011;
          try {
-             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/M226b", "root", "");
+             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/M226b?useSSL=false", "root", "");
              statement = connection.createStatement();
              statement.execute("INSERT INTO GameLists VALUES('" + GameN + "', '" + PubN+ "', 1, 2011);");
          } catch (SQLException e) {
